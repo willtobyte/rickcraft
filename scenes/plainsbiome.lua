bcd466ca3697bc758eb143c78f4b7d972f049cbc
Rodrigo Delduca <rodrigo@delduca.org>
2025-04-25 13:29:48 -0300

    Work in progress

--- scenes/plainsbiome.lua
+++ scenes/plainsbiome.lua
@@ -2,11 +2,22 @@ local scene = {}
 
 local pool = {}
 
+local statemanager = engine:statemanager()
+
 function scene.on_enter()
+  pool.player = scene:get("henrique", SceneType.object)
 end
 
 function scene.on_loop()
+  pool.player.velocity.x = 0
 
+  if statemanager:player(Player.one):on(Controller.left) then
+    pool.player.reflection:set(Reflection.horizontal)
+    pool.player.velocity.x = -360
+  elseif statemanager:player(Player.one):on(Controller.right) then
+    pool.player.reflection:unset()
+    pool.player.velocity.x = 360
+  end
 end
 
 function scene.on_leave()
